cf02411d79c8ed5501d081a4b0dd32f0a4fe85e4
David <david.gieseler@hotmail.com>
2023-01-02 19:09:24 -0300

    Correção validação se nome ja existe

--- src/main/java/br/com/pb/msproduct/application/service/ProductService.java
+++ src/main/java/br/com/pb/msproduct/application/service/ProductService.java
@@ -5,10 +5,14 @@ import br.com.pb.msproduct.application.ports.out.ProductRepository;
 import br.com.pb.msproduct.domain.dto.ProductDTO;
 import br.com.pb.msproduct.domain.dto.ProductResponse;
 import br.com.pb.msproduct.domain.model.Product;
+import br.com.pb.msproduct.framework.exception.DataIntegrityValidationException;
 import br.com.pb.msproduct.framework.exception.IdNotFoundException;
+import br.com.pb.msproduct.framework.exception.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,11 +22,23 @@ public class ProductService implements ProductUseCase {
 
     private final ModelMapper modelMapper;
 
+
+    private void checkIfNameExists(ProductDTO productDTO) {
+        var check = repository.findByName(productDTO.getName());
+        if (check.isPresent()){
+            throw new DataIntegrityValidationException("Product Name already exists");
+        }
+    }
+
     public ProductResponse createProduct(ProductDTO dto) {
+
+        checkIfNameExists(dto);
+
         Product product = modelMapper.map(dto, Product.class);
         repository.save(product);
         return modelMapper.map(product, ProductResponse.class);
     }
+
 
 }
 
