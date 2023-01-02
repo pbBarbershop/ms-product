0027a874ba5a58b25eb53f44d10fda3e97026310
Theodoro Gaspar Ferreira <theodoro.ferreira.pb@compasso.com.br>
2023-01-02 17:29:39 -0300

    commitando o get só para trocar de branch

--- src/main/java/br/com/pb/msproduct/framework/exception/ProductExceptionHandler.java
+++ src/main/java/br/com/pb/msproduct/framework/exception/ProductExceptionHandler.java
@@ -1,16 +1,22 @@
 package br.com.pb.msproduct.framework.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.time.LocalDateTime;
 
 import static org.springframework.http.HttpStatus.*;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ProductExceptionHandler {
 
-    @ExceptionHandler(value ={ProductNotFoundException.class,})
+    @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<Object> handlerProductNotFoundException(ProductNotFoundException productNotFoundException) {
         ProductException productException = new ProductException(
                 productNotFoundException.getMessage(),
@@ -18,5 +24,12 @@ public class ProductExceptionHandler {
                 HttpStatus.NOT_FOUND);
 
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException exception, HttpServletRequest request) {
+        StandartError error = new StandartError
+                (LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), "Wrong value requested in parameter field", request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
